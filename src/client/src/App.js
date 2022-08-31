87921a46db493a547059fc038fd4e98264116b48
Feridun Akpinar <akpinarferidun92@icloud.com>
2022-08-31 22:23:16 +0300

    Add react-router

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useReducer } from 'react';
 import styles from './App.module.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router';
 import { getContext } from './state/context';
 import { initialState } from './state/state';
 import { reducer } from './state/state';
@@ -21,47 +23,21 @@ class App extends React.Component {
     api.setDispatch(this.props.dispatch);
   }
 
-  linkClick = (link) => {
-    switch (link) {
-      case 'Login':
-        this.setState({ activeContainer: 'Default' });
-      case 'Logout':
-        this.props.dispatch({ type: 'LOGOUT' });
-        setTimeout(() => console.log('auth ', this.props.state.auth), 2000);
-        this.setState({ activeContainer: 'Default' });
-    }
-  };
-
-  componentDidUpdate() {
-    if (this.props.state.auth.user && this.state.activeContainer === 'Default'){
-      this.setState({ activeContainer: 'Dashboard' });
-    } 
-  }
-
-  getContainer = () => {
-    if (!this.props.state.auth.user) return <Login {...this.props} />;
-    switch (this.state.activeContainer) {
-      case 'Dashboard':
-        return <Dashboard {...this.props} />;
-      case 'Holons':
-        return <Holons {...this.props} />;
-      case 'Help':
-        return <Help {...this.props} />;
-      case 'Settings':
-        return <Settings {...this.props} />;
-      case 'Account':
-        return <Account {...this.props} />;
-      case 'Default':
-        return <Dashboard {...this.props} />;
-    }
-  };
-
   render() {
     return (
       <App.AppContext.Provider value={{ dispatch: this.props.dispatch }}>
         <div className={styles.app}>
-          <LeftSidebar activeLink={this.state.activeContainer} onLinkClick={this.linkClick} {...this.props} />
-          <div className={styles.container}>{this.getContainer()}</div>
+          <LeftSidebar activeLink={this.state.activeContainer} {...this.props} />
+          <div className={styles.container}>
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard {...this.props} />} />
+                <Route path="/holons" element={<Holons {...this.props} />} />
+                <Route path="/help" element={<Help {...this.props} />} />
+                <Route path="/settings" element={<Settings {...this.props} />} />
+                <Route path="/account" element={<Account {...this.props} />} />
+                <Route path="/" element={<Login {...this.props} />} />
+              </Routes>
+          </div>
         </div>
       </App.AppContext.Provider>
     );
@@ -70,7 +46,9 @@ class App extends React.Component {
 
 const AppWrapper = () => {
   const [state, dispatch] = useReducer(reducer, JSON.parse(JSON.stringify(initialState)));
-  return <App state={state} dispatch={dispatch}></App>;
+  let navigate = useNavigate();
+  let location = useLocation();
+  return <App location={location} navigate={navigate} state={state} dispatch={dispatch}></App>;
 };
 
 export default AppWrapper;
--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -10,6 +11,8 @@ import '@fontsource/roboto/700.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
