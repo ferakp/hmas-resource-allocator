fbdfafc37b7a9e0e64e7153fa631a71dfed73c65
Feridun Akpinar <akpinarferidun92@icloud.com>
2022-10-01 15:57:04 +0300

    Remove old files

--- src/client/test/e2e/account.e2e.js
+++
@@ -1,8 +0,0 @@
-// import * as utils from './utils';
-
-// describe('Account view test', () => {
-//   it('edit own information with given details', async () => {
-//     // edit and save
-//     // check edits
-//   });
-// });
--- src/client/test/e2e/algorithms.e2e.js
+++
@@ -1,7 +0,0 @@
-// import * as utils from './utils';
-
-// describe('Algorithms view test', () => {
-//   it('should have at least three algorithms: FindBestMatch, FindOptimal and FindMaximumExecution', async () => {
-//     // Look for elements whose classes includes "name"
-//   });
-// });
