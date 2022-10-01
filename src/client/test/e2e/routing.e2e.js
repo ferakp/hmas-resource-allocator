d524773b4b6b57130af9c5395272c41f836610fa
Feridun <61657124+ferakp@users.noreply.github.com>
2022-10-01 15:58:17 +0300

    Frontend tests (#35)
    * Fix bug: hide elements when screen size is too small
    
    * Add tests for holons and tasks
    
    * Remove old files

--- src/client/test/e2e/routing.e2e.js
+++ src/client/test/e2e/routing.e2e.js
@@ -1,19 +1,11 @@
+import * as utils from './utils';
+
 describe('Routing test', () => {
-  const logout = async () => {
-    const childLogout = await $('p*=Logout');
-    const parentLogout = childLogout.parentElement();
-    await parentLogout.click();
-  };
 
   it('should have / route for login and login with the username admin and the password password', async () => {
-    await browser.url(`http://localhost:3000/`);
-
-    await $('#usernameInput').setValue('admin');
-    await $('#passwordInput').setValue('password');
-    await $('button[type="button"]').click();
-
+    await utils.login();
     await $('p[class*="Dashboard"]').waitForExist({ timeout: 4000 });
-    await logout();
+    await utils.logout();
   });
 
   it('should have open /api route', async () => {
@@ -31,68 +23,48 @@ describe('Routing test', () => {
   });
 
   it('should redirect to /dashboard/analytics after login', async () => {
-    await browser.url(`http://localhost:3000/`);
-
-    await $('#usernameInput').setValue('admin');
-    await $('#passwordInput').setValue('password');
-    await $('button[type="button"]').click();
-
+    await utils.login();
     await $('p[class*="Analytics"]').waitForExist({ timeout: 4000 });
-    await logout();
+    await utils.logout();
   });
 
   it('should have the route /dashboard/tasks', async () => {
-    await browser.url(`http://localhost:3000/`);
-    await $('#usernameInput').setValue('admin');
-    await $('#passwordInput').setValue('password');
-    await $('button[type="button"]').click();
+    await utils.login();
     await $('p[class*="Analytics"]').waitForExist({ timeout: 4000 });
     await browser.url(`http://localhost:3000/dashboard/tasks`);
     await $('p[class*="Tasks"]').waitForExist({ timeout: 4000 });
-    await logout();
+    await utils.logout();
   });
 
   it('should have the route /dashboard/holons', async () => {
-    await browser.url(`http://localhost:3000/`);
-    await $('#usernameInput').setValue('admin');
-    await $('#passwordInput').setValue('password');
-    await $('button[type="button"]').click();
+    await utils.login();
     await $('p[class*="Analytics"]').waitForExist({ timeout: 4000 });
     await browser.url(`http://localhost:3000/dashboard/holons`);
     await $('p[class*="Holons"]').waitForExist({ timeout: 4000 });
-    await logout();
+    await utils.logout();
   });
 
   it('should have the route /dashboard/allocations', async () => {
-    await browser.url(`http://localhost:3000/`);
-    await $('#usernameInput').setValue('admin');
-    await $('#passwordInput').setValue('password');
-    await $('button[type="button"]').click();
+    await utils.login();
     await $('p[class*="Analytics"]').waitForExist({ timeout: 4000 });
     await browser.url(`http://localhost:3000/dashboard/allocations`);
     await $('p[class*="Allocations"]').waitForExist({ timeout: 4000 });
-    await logout();
+    await utils.logout();
   });
 
   it('should have the route /dashboard/algorithms', async () => {
-    await browser.url(`http://localhost:3000/`);
-    await $('#usernameInput').setValue('admin');
-    await $('#passwordInput').setValue('password');
-    await $('button[type="button"]').click();
+    await utils.login();
     await $('p[class*="Analytics"]').waitForExist({ timeout: 4000 });
     await browser.url(`http://localhost:3000/dashboard/algorithms`);
     await $('p[class*="Algorithms"]').waitForExist({ timeout: 4000 });
-    await logout();
+    await utils.logout();
   });
 
   it('should have the route /account', async () => {
-    await browser.url(`http://localhost:3000/`);
-    await $('#usernameInput').setValue('admin');
-    await $('#passwordInput').setValue('password');
-    await $('button[type="button"]').click();
+    await utils.login();
     await $('p[class*="Analytics"]').waitForExist({ timeout: 4000 });
     await browser.url(`http://localhost:3000/account`);
-    await $('p[class*="header"]').waitForExist({ timeout: 4000 });
-    await logout();
+    await $('div[class*="header"]').waitForExist({ timeout: 4000 });
+    await utils.logout();
   });
 });
--- src/client/wdio.conf.js
+++ src/client/wdio.conf.js
@@ -54,10 +54,15 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
+        maxInstances: 3,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
+        
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu'],
+          },
+
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
@@ -141,7 +146,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 120000
     },
     //
     // =====
